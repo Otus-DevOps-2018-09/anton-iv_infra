48ec47099044e458f4b17086c53f8440320ca648
Ivashkevich Anton <ivashkevich.anton@gmail.com>
2018-10-19 12:04:24 +0300

    puma.service corrected --dir

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -3,5 +3,7 @@ git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 
 mv /home/gceuser/puma.service /etc/systemd/system/puma.service
+chown root:root /etc/systemd/system/puma.service 
 chmod 664 /etc/systemd/system/puma.service
+systemctl daemon-reload
 systemctl enable puma.service
