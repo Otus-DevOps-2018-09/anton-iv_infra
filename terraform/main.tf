33f2b97e84cbbc02dba38d0167ff62b9b534a781
Anton Ivashkevich <anton.ivashkevich@gmail.com>
2018-10-24 20:51:34 +0300

    terraform fmt applied

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,60 +1,71 @@
 provider "google" {
-    version = "1.4.0"
-    #project = "infra-219514"
-    #region = "europe-west1"
-    project = "${var.project}"   
-    region  = "${var.region}" 
+  version = "1.4.0"
+
+  #project = "infra-219514"
+  #region = "europe-west1"
+  project = "${var.project}"
+
+  region = "${var.region}"
 }
 
 resource "google_compute_instance" "app" {
-    name = "reddit-app"
-    machine_type = "f1-micro"
-    zone = "${var.zone}"
-    metadata {    
-        #ssh-keys = "gceuser:${file("~/.ssh/gceuser.pub")}"   
-        ssh-keys = "gceuser:${file(var.public_key_path)}" 
-    } 
-    # определение загрузочного диска
-    boot_disk {
-        initialize_params {
-            #image = "reddit-base"
-            image = "${var.disk_image}" 
-        }
-    }
-    # определение сетевого интерфейса
-    network_interface {
-        # сеть, к которой присоединить данный интерфейс
-        network = "default"
-        # использовать ephemeral IP для доступа из Интернет
-        access_config {}
-    }  
-    tags = ["reddit-app"] 
-    
-    connection {     
-        type     = "ssh"
-        user     = "gceuser"
-        agent = false
-        private_key = "${file(var.private_key_path)}"   
-    }
+  name         = "reddit-app"
+  machine_type = "f1-micro"
+  zone         = "${var.zone}"
 
-    provisioner "file" {
-        source = "files/puma.service"
-        destination = "/tmp/puma.service" 
+  metadata {
+    #ssh-keys = "gceuser:${file("~/.ssh/gceuser.pub")}"   
+    ssh-keys = "gceuser:${file(var.public_key_path)}"
+  }
+
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      #image = "reddit-base"
+      image = "${var.disk_image}"
     }
-    provisioner "remote-exec" {   
-        script = "files/deploy.sh" 
-    }
+  }
+
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+
+  tags = ["reddit-app"]
+
+  connection {
+    type        = "ssh"
+    user        = "gceuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
 }
 
-resource "google_compute_firewall" "firewall_puma" {  
-    name    = "allow-puma-default" # Название сети, в которой действует правило   
-    network = "default" # Какой доступ разрешить   
-    allow {    
-         protocol = "tcp"    
-         ports    = ["9292"]  
-    } 
-    # Каким адресам разрешаем доступ   
-    source_ranges = ["0.0.0.0/0"] 
-    # Правило применимо для инстансов с перечисленными тэгами   
-    target_tags = ["reddit-app"] 
+resource "google_compute_firewall" "firewall_puma" {
+  name    = "allow-puma-default" # Название сети, в которой действует правило   
+  network = "default"            # Какой доступ разрешить   
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Каким адресам разрешаем доступ   
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с перечисленными тэгами   
+  target_tags = ["reddit-app"]
 }
