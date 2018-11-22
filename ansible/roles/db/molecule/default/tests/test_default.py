14183c8d1ddcb3712da54de6d3108d58ea12f224
Ivashkevich Anton <ivashkevich.anton@gmail.com>
2018-11-22 20:12:10 +0300

    HW11

--- ansible/roles/db/molecule/default/tests/test_default.py
+++ ansible/roles/db/molecule/default/tests/test_default.py
@@ -17,3 +17,7 @@ def test_config_file(host):
     config_file = host.file('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
+
+# check if MongoDB is running on 27017 port
+def test_mongo_port_listening(host):
+    port = host.socket('tcp://0.0.0.0:27017')
